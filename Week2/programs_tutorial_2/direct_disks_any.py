6dd02129fabbf4969861af87740ed88da4b97fd2
gavin <gavinconran@yahoo.co.uk>
2016-05-31 14:06:47 +0100

    added notes on numerical method to direct_disks_any.py

--- Week2/programs_tutorial_2/direct_disks_any.py
+++ Week2/programs_tutorial_2/direct_disks_any.py
@@ -17,8 +17,10 @@ pairs = [(i, j) for i in range(N - 1) for j in range(i + 1, N)]
 eta_max_list = []
 # Compute P_accept using numerical evaluation of p_accept
 for conf in xrange(n_confs):
+    # Compute a configuration
     L = [(random.random(), random.random()) for k in range(N)]
     sigma_max = min(dist(L[i], L[j]) for i, j in pairs) / 2.0
+    # compute density (eta_max) from N * pi * sigma**2 (see p.3 of tutorial2 notes)
     eta_max = N * math.pi * sigma_max ** 2
     eta_max_list.append(eta_max)
 
@@ -27,6 +29,7 @@ pylab.figure()
 n, bins, patches = pylab.hist(eta_max_list, 100, histtype='step', cumulative=-1, 
                    log=True, normed=True, label="numerical evaluation of p_accept")
 # Compute P_accept using 1st   order Virial expansion --> exp(-2(N-1)*eta)
+# see p.5 of my tutorial2 notes
 explaw = [math.exp( - 2.0 * (N - 1) * eta) for eta in bins]
 pylab.plot(bins, explaw, 'r--', linewidth=1.5, label="1st order virial expansion")
 pylab.xlabel('density eta')
