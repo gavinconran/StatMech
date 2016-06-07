719bdd51af1ae03970deb7d35d69903fb2a1f241
gavin <gavinconran@yahoo.co.uk>
2016-06-07 12:50:14 +0100

    went throught numerical and analytical solutions for calculatint eta

--- Week3/programs_tutorial_3/direct_pins_density.py
+++ Week3/programs_tutorial_3/direct_pins_density.py
@@ -1,11 +1,13 @@
 import pylab
 
+# returns the combinatorial factor giving the number of choices
 def binomialCoeff(n, k):
     result = 1
     for i in range(1, k+1):
         result = result * (n-i+1) / i
     return result
 
+# returns the partition function value calculated using Virial Expansion
 def Z(N, L, sigma):
     freespace = L - 2.0 * N * sigma
     if freespace > 0.0:
@@ -14,19 +16,22 @@ def Z(N, L, sigma):
         result = 0.0
     return result
 
+# computeds the probbaility of having  apin at centre x
 def pi(x, N, L, sigma):
     tot = 0.
     for k in range(0, N):
-        Z1 = Z(k, x - sigma, sigma)
-        Z2 = Z(N - k - 1, L - x - sigma, sigma)
-        tot += binomialCoeff( N - 1, k) * Z1 * Z2
+        Z1 = Z(k, x - sigma, sigma) # partition function for LHS
+        Z2 = Z(N - k - 1, L - x - sigma, sigma) # partition function for RHS
+        tot += binomialCoeff( N - 1, k) * Z1 * Z2 # compute combinatorial factor
+    # sum all values from k=0 to k=N-1
     Ztotal = Z(N, L, sigma)
-    return tot / Ztotal
+    return tot / Ztotal # return probability of pin at centre x
 
 L = 20.0
 N = 10
 sigma = 0.75
 xr = pylab.linspace(0.0, L, 201)
+# compute densities for each position x given in xr
 yr = [pi(x, N, L, sigma) for x in xr]
 pylab.plot(xr, yr, 'red', linewidth=2.0)
 pylab.xlabel('$x$', fontsize=14)
--- Week3/programs_tutorial_3/direct_pins_noreject.py
+++ Week3/programs_tutorial_3/direct_pins_noreject.py
@@ -5,6 +5,9 @@ L = 20.0
 sigma = 0.75
 n_runs = 800
 for run in range(n_runs):
+    # random selct poistions on the deflated line
     y = [random.uniform(0.0, L - 2 * N * sigma) for k in range(N)]
+    # sort works for 1-D but maked algorithm redundant for more than 1-D
     y.sort()
+    # Take care of inflation
     print [y[i] + (2 * i + 1) * sigma for i in range(N)]
