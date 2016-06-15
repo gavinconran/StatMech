c3721429b20c9cae157d43ab42ab9cf831100015
gavin <gavinconran@yahoo.co.uk>
2016-06-15 20:14:39 +0100

    played with gamma nd gauss transform programs

--- Week4/programs_tutorial_4/gamma_transform.py
+++ Week4/programs_tutorial_4/gamma_transform.py
@@ -1,7 +1,23 @@
-import random
+# allows for direct sampling of a divergent distribution
+# uses sample transformation from
+# a uniform distribution to the divergent distribution x**gamma
+
+import random, pylab,math
 
 gamma = -0.5
+data = []
 n_trials = 10000
 for trial in xrange(n_trials):
     x = (random.uniform(0.0, 1.0)) ** (1.0 / (gamma + 1.0))
-    print x
+    data.append(x)
+    #print x
+
+
+pylab.hist(data, bins=100, normed='True')
+x = [a / 100.0 for a in xrange(1, 100)]
+y = [1.0 / (2.0 * math.sqrt(a)) for a in x]
+pylab.plot(x, y, 'red', linewidth = 2)
+pylab.title('Theoretical distribution $\pi(x)={1}/{(2 \sqrt{x})}$ and normalized',fontsize=16)
+pylab.xlabel('$x$', fontsize=18)
+pylab.ylabel('$\pi(x)$', fontsize=18)
+pylab.show()
--- Week4/programs_tutorial_4/gauss_transform.py
+++ Week4/programs_tutorial_4/gauss_transform.py
@@ -1,8 +1,25 @@
-import scipy.special, random, math
+# transforms a uniformly generated randomvariable (Upsilon) into
+# an "x" position on the x-axis of the gaussian PDF 
+
+import scipy.special, random, math, pylab
 
 n_trials = 100000
+data = []
 for trial in xrange(n_trials):
     Upsilon = random.uniform(0.0, 1.0)
+    # calculate x from the inverse error function
     x = math.sqrt(2.0) * scipy.special.erfinv(2.0 * Upsilon - 1.0)
-    print x
+    data.append(x)
+    #print x
+
+
+pylab.hist(data, bins=100, normed='True')
+pylab.xlabel('$x$', fontsize=16)
+pylab.ylabel('$\pi(x)$', fontsize=16)
+#x = [a / 100.0 for a in xrange(1, 101)]
+#y = [0.5 / math.sqrt(a) for a in x]
+#pylab.plot(x, y, linewidth=1.5, color='r')
+pylab.title('Theoretical distribution $\pi(x)$\
+    \n histogram for '+str(len(data))+' samples',fontsize=16)
+pylab.show()
 
