574e01cfc0b069002740f835706e3b9cdda5c8cf
gavin <gavinconran@yahoo.co.uk>
2016-06-21 11:57:36 +0100

    took apart harmonic_wavefunction.py

--- Week5/programs_lecture_5/harmonic_wavefunction.py
+++ Week5/programs_lecture_5/harmonic_wavefunction.py
@@ -1,11 +1,17 @@
 # implements exact recursion reeelation for the hermite polynomials
-# Solves Schroinger equation, i.e. Psi 
+# Solves Schroinger equation, i.e. returns the function Psi(x) for values of x
 
-import math
+import math, pylab
 
+# construct waveforms
 n_states = 4
 grid_x = [i * 0.2 for i in range(-25, 26)]
 psi = {}
+# for each value of x in grid_x
+# compute for each of the Energy levels from 0 to n_states-1
+# store results in a directionary
+# Each key of dictionary is a point x on the grid and
+# the value of each element in the dictionary is a list of the computed Enery levels 
 for x in grid_x:
     psi[x] = [math.exp(-x ** 2 / 2.0) / math.pi ** 0.25]  # ground state
     psi[x].append(math.sqrt(2.0) * x * psi[x][0])         # first excited state
@@ -15,3 +21,17 @@ for x in grid_x:
                       math.sqrt((n - 1.0) / n) * psi[x][n - 2])
 for n in range(n_states):
     print 'level %i:' % n, [psi[x][n] for x in grid_x]
+
+## Plot Psi(x)
+x = grid_x
+y = [psi[p][0] for p in psi.keys()] 
+pylab.plot(x, y, 'k', lw=1.5)
+y = [psi[p][1] for p in psi.keys()] 
+pylab.plot(x, y, 'b', lw=1.5)
+y = [psi[p][2] for p in psi.keys()] 
+pylab.plot(x, y, 'r', lw=1.5)
+y = [psi[p][3] for p in psi.keys()] 
+pylab.plot(x, y, 'g', lw=1.5)
+pylab.show()
+
+
