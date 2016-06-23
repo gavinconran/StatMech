6ed5a7ede579c8d512879e785626a2b3ae9624ca
gavin <gavinconran@yahoo.co.uk>
2016-06-23 21:16:44 +0100

    A2.py working and complete(Beta=2 still skewed

--- Week5/assignment/A/A2.py
+++ Week5/assignment/A/A2.py
@@ -9,7 +9,7 @@ import random, math, pylab
 # Used fros STEP 1
 # n state wave function
 def psi_n(x, n):
-    return (1 / math.pi**0.25) * math.exp(-x**2/float(2))
+    return (1 / math.pi**0.25) * math.exp(-x**2/float(2)) #math.exp(-beta * n)   #
 
 # used for STEP 2
 def psi_n_square(x, n):
@@ -36,22 +36,22 @@ def prob_classic(x, Beta):
     return math.sqrt(Beta/ (2*math.pi)) * math.exp(-Beta * x**2/float(2)) 
 
 # Helper function for A2 step 1
-def step1(x_new, x, n):
-    return min(1, (psi_n(x_new, n) / psi_n(x, n)))**2
+def step1(x_new, x, n, Beta):
+    return min(1, (psi_n_square(x_new, n) / psi_n_square(x, n))) #**2
 
 # Helper function for A2 step 2
-def step2(x_new, x, n_new, n, Beta):
-    return min(1, (psi_n_square(x_new, n) / psi_n_square(x, n)) * math.exp(-Beta * (E_n(n_new) - E_n(n))))
+def step2(psi_x_n_new_sq, x_new, x, n_new, n, Beta):
+    return min(1, (psi_x_n_new_sq / psi_n_square(x, n)) * math.exp(-Beta * (E_n(n_new) - E_n(n))))
 
 
 # Beta is proportional to 1 / T
 BetaList = [5, 1 , 0.2] 
-n_trials = 100000
+n_trials = 10000000
 for Beta in BetaList:
     # Initialise variables
     x = 0.0
     delta = 0.1
-    n = 0
+    n = 2
     data = []
     # apply markov-chain Monte carlo sampling with Metropolis acceptance
     # to approximate the finte temperature quantaum probability distribution
@@ -59,24 +59,30 @@ for Beta in BetaList:
         x_new = x + random.uniform(-delta, delta)
 
         # step 1: move (n,x) to (n, x')
-        if random.uniform(0.0, 1.0) < step1(x_new, x, n):
+        if random.uniform(0.0, 1.0) < step1(x_new, x, n, Beta):
             x = x_new 
+            #print "step 1"
 
         # step 2:
         m = random.randrange(-1, 2, 2)
         n_new = n + m
+        
 
 #Notice that this function returns 0 when n=-1. This is useful to simplify the acceptance probability for the n-changing move, #since we want to reject all moves that go from n=0 to n=-1)
-
-        if m > 0 and psi_n_square(x, n) > 0 and random.uniform(0.0, 1.0) <  step2(x_new, x, n_new, n, Beta):    
-            n = n_new
-    
+        psi_x_n_new_sq =  psi_n_square(x, n_new)
+        #if n == 0 and  and psi_x_n_new_sq > 0 and 
+        if not(n == 0 and psi_x_n_new_sq == 0):
+            if random.uniform(0.0, 1.0) <  \
+                           step2(psi_x_n_new_sq, x_new, x, n_new, n, Beta):    
+                n = n_new
+                #print "step 2"
+        #print "n = ", n
         data.append(x)
 
     # Create Graphic
     pylab.figure()
     pylab.hist(data, 100, normed = 'True')
-    x = [a/float(1000) for a in range(-3000, 3000)]
+    x = [a/float(1000) for a in range(-6000, 6000)]
     # Plot exact Quantum Probability Distribution
     y_quant = [prob_quant(a, Beta) for a in x] 
     pylab.plot(x, y_quant, c='red', linewidth=2.0, label='Quantum')
