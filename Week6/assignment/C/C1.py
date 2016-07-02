64ce052f373f06f8bd78e75062208ef5ed3bb430
gavin <gavinconran@yahoo.co.uk>
2016-07-02 14:04:06 +0100

    C1.py

---
+++ Week6/assignment/C/C1.py
@@ -0,0 +1,81 @@
+# C1.py
+# extension of B2_levy_harmonic_path.py
+# variant levy_harmonic_path.py
+# Markov-chain using partial freezing" + "direct sampling
+
+import math, random, pylab
+
+# Step 1: Incorporate a function V(x, cubic, quartic)
+def V(x, cubic, quartic):
+    pot = x ** 2 / 2.0 + cubic * x ** 3 + quartic * x ** 4
+    return pot
+
+# This function (which returns a list of N elements) 
+# should replace the naive construction for the N elements of naive_harmonic_path                            
+def levy_harmonic_path(xstart, xend, dtau, N):
+    x = [xstart]
+    for k in range(1, N):
+        dtau_prime = (N - k) * dtau
+        Ups1 = 1.0 / math.tanh(dtau) + \
+               1.0 / math.tanh(dtau_prime)
+        Ups2 = x[k - 1] / math.sinh(dtau) + \
+               xend / math.sinh(dtau_prime)
+        x.append(random.gauss(Ups2 / Ups1, \
+               1.0 / math.sqrt(Ups1)))
+    return x
+
+# It is important to realize that the rho_free part of the free density matrix is no longer in the Metropolis step.
+# Therefore, you should replace the harmonic Levy construction of section B2 by the free Levy construction, 
+# but new paths should be accepted with the Metropolis algorithm.
+def levy_free_path(xstart, xend, dtau, N):
+    x = [xstart]
+    for k in range(1, N):
+        dtau_prime = (N - k) * dtau
+        x_mean = (dtau_prime * x[k - 1] + dtau * xend) / \
+                 (dtau + dtau_prime)
+        sigma = math.sqrt(1.0 / (1.0 / dtau + 1.0 / dtau_prime))
+        x.append(random.gauss(x_mean, sigma))
+    return x
+
+# Initialise variables
+beta = 1.0 #20.0
+N = 80
+dtau = beta / N
+x = [5.0] * N
+data = []
+Ncut = N/2
+cubic = 0
+quartic = 0
+
+Weight_old = math.exp(sum(-V(a, cubic, quartic) * dtau for a in x))
+
+n_steps = 400000
+for step in range(n_steps):
+    # Step 2: Construct a new path (x_new) between x[0] and x[0], from Levy_free_path.
+    x_new = levy_free_path(x[0], x[0], dtau, N)
+    # Step 3: Compute its Trotter weight, the part of the statistical weight not yet taken into account by the path construction
+    Weight_new = math.exp(sum(-V(a, cubic, quartic) * dtau for a in x_new))
+    # Step 4: Accept the new path with probability min(1, Weight_new/ Weight_old)
+    if random.uniform(0.0, 1.0) < min(1, Weight_new/ Weight_old):
+        # Update the Weight_old
+        Weight_old = Weight_new + math.exp(sum(-V(a, cubic, quartic) * dtau for a in x))
+        # Update the path
+        x = x_new[:]
+        # Wrap the path
+        x = x[1:] + x[:1]
+    k = random.randint(0, N - 1)
+    data.append(x[k])   
+
+pylab.hist(data, normed=True, bins=100, label='QMC')
+list_x = [0.1 * a for a in range (-30, 31)]
+list_y = [math.sqrt(math.tanh(beta / 2.0)) / math.sqrt(math.pi) * \
+          math.exp(-x ** 2 * math.tanh(beta / 2.0)) for x in list_x]
+pylab.plot(list_x, list_y, label='analytic')
+pylab.legend()
+pylab.xlabel('$x$')
+pylab.ylabel('$\\pi(x)$ (normalized)')
+pylab.title('levy_free_path (beta=%s, N=%i)' % (beta, N))
+pylab.xlim(-2, 2)
+pylab.savefig('plot_C1_beta%s.png' % beta)
+pylab.show()
+
