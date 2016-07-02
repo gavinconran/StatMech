ec48ed00db097aa8d05a873bf38e68bedfe4d3bf
gavin <gavinconran@yahoo.co.uk>
2016-07-02 17:37:35 +0100

    C1.py and C2.py working and complete

--- Week6/assignment/C/C2.py
+++ Week6/assignment/C/C2.py
@@ -1,13 +1,15 @@
-# C1.py
+# C2.py
 # extension of B2_levy_harmonic_path.py
 # variant levy_harmonic_path.py
 # Markov-chain using partial freezing" + "direct sampling
 
 import math, random, pylab
 
-# Step 1: Incorporate a function V(x, cubic, quartic)
-def V(x, cubic, quartic):
-    pot = x ** 2 / 2.0 + cubic * x ** 3 + quartic * x ** 4
+
+# Step 1: Incorporate a function V_anharm(x, cubic, quartic)
+# does not include harmonic part x**2
+def V_anharm(x, cubic, quartic):
+    pot = cubic * x ** 3 + quartic * x ** 4
     return pot
 
 # This function (which returns a list of N elements) 
@@ -41,25 +43,24 @@ def levy_free_path(xstart, xend, dtau, N):
 beta = 20.0
 N = 100
 dtau = beta / N
-# changed initial value of x
-x = levy_harmonic_path(5.0, 5.0, dtau, N) #[5.0] * N #levy_free_path(5.0, 5.0, dtau, N)  #[5.0] * N
+x = levy_harmonic_path(5.0, 5.0, dtau, N)
 data = []
 Ncut = N/20
-cubic = -1.0 #0 #-1.0
-quartic = 1.0 #0 #1.0 # 0
+cubic = -1.0
+quartic = 1.0 # 0
 accept = 0
-Weight_old = math.exp(sum(-V(a, cubic, quartic) * dtau for a in x))
+Weight_old = math.exp(sum(-V_anharm(a, cubic, quartic) * dtau for a in x))
 
 n_steps = 400000
 for step in range(n_steps):
     # Step 2: Construct a new path (x_new) between x[0] and x[0], from Levy_free_path.
-    x_new = levy_free_path(x[0], x[Ncut], dtau, Ncut) + x[Ncut:]  
+    x_new = levy_harmonic_path(x[0], x[Ncut], dtau, Ncut) + x[Ncut:]  
     # Step 3: Compute its Trotter weight, the part of the statistical weight not yet taken into account by the path construction
-    Weight_new = math.exp(sum(-V(a, cubic, quartic) * dtau for a in x_new))
+    Weight_new = math.exp(sum(-V_anharm(a, cubic, quartic) * dtau for a in x_new))
     # Step 4: Accept the new path with probability min(1, Weight_new/ Weight_old)
     if random.uniform(0.0, 1.0) < min(1, Weight_new/ Weight_old):
         # Update the Weight_old
-        Weight_old = math.exp(sum(-V(a, cubic, quartic) * dtau for a in x)) + Weight_new
+        Weight_old = math.exp(sum(-V_anharm(a, cubic, quartic) * dtau for a in x)) + Weight_new
         #print "Weight_old: ", Weight_old
         # Update the path
         x = x_new[:]
@@ -80,11 +81,11 @@ pylab.legend()
 pylab.xlabel('$x$')
 pylab.ylabel('$\\pi(x)$ (normalized)')
 
-pylab.title('Levy Free Path (Beta=%s, N=%i) ' %(beta, N) + \
+pylab.title('Levy Harmonic Path (Beta=%s, N=%i) ' %(beta, N) + \
             '\nNormalized Histogram (Cubic=%s, Quartic=%s) ' % (cubic, quartic), fontsize = 18)
 
 #pylab.title('levy_free_path (beta=%s, N=%i)' % (beta, N))
 pylab.xlim(-2, 2)
-pylab.savefig('plot_C1_beta%s.png' % beta)
+pylab.savefig('plot_C2_beta%s.png' % beta)
 pylab.show()
 
