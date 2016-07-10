c9cf80e0d6bb75b004a4ed21e7975a6bf51551b2
gavin <gavinconran@yahoo.co.uk>
2016-07-10 21:19:11 +0100

    added notes to cluster_ising.py

--- Week8/programs_lecture_8/cluster_ising.py
+++ Week8/programs_lecture_8/cluster_ising.py
@@ -1,3 +1,7 @@
+# cluster_ising.py
+# much faster than local monte carlo algo
+# speed up increases with system size
+
 import random, math
 
 L = 100
@@ -10,16 +14,28 @@ p  = 1.0 - math.exp(-2.0 / T)
 nsteps = 10000
 S = [random.choice([1, -1]) for k in range(N)]
 for step in range(nsteps):
+    # take a random spin
     k = random.randint(0, N - 1)
+    # place random spin into cluster and into the pocket
     Pocket, Cluster = [k], [k]
     while Pocket != []:
+        # take a random element from the cluster
         j = random.choice(Pocket)
+        # check all of the element's neighbours
         for l in nbr[j]:
+            # for each of the enighbours check to see if NOT already in the cluster
+            # whether it has the same sign AND
+            # a random number between 0 and 1 is smaller than p 
             if S[l] == S[j] and l not in Cluster \
                    and random.uniform(0.0, 1.0) < p:
+                # if all 3 conditions are satisfied
+                # add the new spin to the cluster and the pocket
                 Pocket.append(l)
                 Cluster.append(l)
+        # remove new spin from the pocket
         Pocket.remove(j)
+    # once the pocket is empty
+    # flip the entire cluster
     for j in Cluster:
         S[j] *= -1
 
